0e3f23740a210f98f399a4fa546efdd331932da6
munshiahmedi <ahmedimunshi@gmail.com>
2026-02-13 18:26:11 +0530

    Update client configuration and remove unused files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,42 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { visualizer } from 'rollup-plugin-visualizer';
-import viteCompression from 'vite-plugin-compression';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteCompression({
-      algorithm: 'brotliCompress',
-      ext: '.br',
-      threshold: 1024, // Only files larger than 1kb will be compressed
-    }),
-    visualizer({
-      open: false,
-      gzipSize: true,
-      brotliSize: true,
-    }),
-  ],
-  build: {
-    minify: 'esbuild',
-    cssMinify: true,
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom', 'react-router-dom'],
-          vendor: ['framer-motion'],
-          icons: ['react-icons'],
-        },
-      },
-    },
-  },
-  server: {
-    hmr: {
-      overlay: false,
-    },
-  },
-  preview: {
-    port: 3000,
-  },
+  plugins: [react()],
 });
